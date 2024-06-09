a40ebf98a6cf8d89c27afcd794c8efea52bec65a
Jan <jan@nedralia.se>
2024-06-09 09:48:27 +0200

    feat: Add support for associating comments with specific posts

--- contracts/Comments.sol
+++ contracts/Comments.sol
@@ -1,26 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-// TODO: Make the comment be tied to a specific post
-
 contract Comments {
   struct Comment {
     string text;
     address author;
   }
 
-  Comment[] public comments;
+  // Mapping from post ID to comments
+  mapping(uint => Comment[]) public comments;
 
-  function addComment(string memory _text) public {
-    comments.push(Comment(_text, msg.sender));
+  function addComment(uint _postId, string memory _text) public {
+    comments[_postId].push(Comment(_text, msg.sender));
   }
 
-  function getComment(uint _index) public view returns (string memory, address) {
-    Comment memory comment = comments[_index];
+  function getComment(uint _postId, uint _index) public view returns (string memory, address) {
+    Comment memory comment = comments[_postId][_index];
     return (comment.text, comment.author);
   }
 
-  function getCommentsCount() public view returns (uint) {
-    return comments.length;
+  function getCommentsCount(uint _postId) public view returns (uint) {
+    return comments[_postId].length;
   }
 }
