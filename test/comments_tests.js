2f89a6b85ce990ffaf12bb3ff9f316ec70dc3d3e
Jan <jan@nedralia.se>
2024-06-09 10:11:07 +0200

    chore: Update Comments contract tests and add support for associating comments with specific posts

--- test/comments_tests.js
+++ test/comments_tests.js
@@ -9,10 +9,20 @@ contract("CommentsTests", function ( accounts ) {
 
   it("should add a comment", async () => {
     const instance = await Comments.new();
-    await instance.addComment("Hello, World!", { from: accounts[0] });
-    const comment = await instance.getComment(0);
+    await instance.addComment(0, "Hello, World!", { from: accounts[0] });
+    const comment = await instance.getComment(0, 0);
     assert.equal(comment[0], "Hello, World!");
     assert.equal(comment[1], accounts[0]);
+  });
+
+  it("should add a comment on one post without affecting another post", async () => {
+    const instance = await Comments.new();
+    await instance.addComment(0, "Hello, World!", { from: accounts[0] });
+    await instance.addComment(1, "Hello, Universe!", { from: accounts[0] });
+    const comment1 = await instance.getComment(0, 0);
+    const comment2 = await instance.getComment(1, 0);
+    assert.equal(comment1[0], "Hello, World!");
+    assert.equal(comment2[0], "Hello, Universe!");
   });
 
   it('should cost some ethereum to add a comment', async () => {
@@ -22,7 +32,7 @@ contract("CommentsTests", function ( accounts ) {
     const balanceBefore = await web3.eth.getBalance(accounts[0]);
 
     // Add a comment
-    await instance.addComment("Hello, World!", { from: accounts[0] });
+    await instance.addComment(111, "Hello, World!", { from: accounts[0] });
 
     // Get the balance of the first account after adding a comment
     const balanceAfter = await web3.eth.getBalance(accounts[0]);
@@ -32,27 +42,27 @@ contract("CommentsTests", function ( accounts ) {
 
   it('should not cost any ethereum to get a comment', async () => {
     const instance = await Comments.new();
-    await instance.addComment("Hello, World!", { from: accounts[0] });
+    await instance.addComment(1111, "Hello, World!", { from: accounts[0] });
     const balanceBefore = await web3.eth.getBalance(accounts[0]);
-    await instance.getComment(0);
+    await instance.getComment(1111, 0);
     const balanceAfter = await web3.eth.getBalance(accounts[0]);
     assert(balanceBefore == balanceAfter);
   });
 
   it('should get the comment by index', async () => {
     const instance = await Comments.new();
-    await instance.addComment("Hello, World!", { from: accounts[0] });
-    await instance.addComment("Hello, Universe!", { from: accounts[0] });
-    const comment = await instance.getComment(1);
+    await instance.addComment(99, "Hello, World!", { from: accounts[0] });
+    await instance.addComment(99, "Hello, Universe!", { from: accounts[0] });
+    const comment = await instance.getComment(99, 1);
     assert.equal(comment[0], "Hello, Universe!");
   });
 
   it('should get the number of comments', async () => {
     const instance = await Comments.new();
-    await instance.addComment("Awesome post", { from: accounts[0] });
-    await instance.addComment("Love the content!", { from: accounts[0] });
-    await instance.addComment("Wow, such wow!", { from: accounts[0] });
-    const numComments = await instance.getCommentsCount();
+    await instance.addComment(404, "Awesome post", { from: accounts[0] });
+    await instance.addComment(404, "Love the content!", { from: accounts[0] });
+    await instance.addComment(404, "Wow, such wow!", { from: accounts[0] });
+    const numComments = await instance.getCommentsCount(404);
     assert.equal(numComments, 3);
   });
 });
